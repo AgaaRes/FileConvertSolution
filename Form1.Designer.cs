766455b3bd3b8bb4d030b6d40cd9b96701e1dcfa
Ky <qghe03205@gmail.com>
2025-12-14 00:26:09 +0700

    Add PDF→DOCX convert feature and update Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -6,8 +6,8 @@ namespace FileConverterGUI.Converters
     partial class Form1
     {
         private ComboBox cbConvertType;
-        private Button btnChooseFile;
-        private Button btnConvert;
+        private Button BtnChooseFile;
+        private Button BtnConvert;
         private Label lblFile;
         private System.ComponentModel.IContainer components = null;
 
@@ -21,58 +21,68 @@ namespace FileConverterGUI.Converters
         private void InitializeComponent()
         {
             cbConvertType = new ComboBox();
-            btnChooseFile = new Button();
-            btnConvert = new Button();
+            BtnChooseFile = new Button();
+            BtnConvert = new Button();
             lblFile = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
             // cbConvertType
             // 
             cbConvertType.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbConvertType.Items.AddRange(new object[] { "DOCX → PDF", "TXT → PDF" });
-            cbConvertType.Location = new Point(40, 22);
+            cbConvertType.Items.AddRange(new object[] { "DOCX → PDF", "PDF → DOCX", "TXT → PDF" });
+            cbConvertType.Location = new Point(135, 6);
             cbConvertType.Name = "cbConvertType";
-            cbConvertType.Size = new Size(200, 23);
+            cbConvertType.Size = new Size(200, 28);
             cbConvertType.TabIndex = 0;
             // 
-            // btnChooseFile
+            // BtnChooseFile
             // 
-            btnChooseFile.Location = new Point(12, 69);
-            btnChooseFile.Name = "btnChooseFile";
-            btnChooseFile.Size = new Size(258, 57);
-            btnChooseFile.TabIndex = 1;
-            btnChooseFile.Text = "Chọn file";
-            btnChooseFile.Click += btnChooseFile_Click;
+            BtnChooseFile.Location = new Point(317, 93);
+            BtnChooseFile.Name = "BtnChooseFile";
+            BtnChooseFile.Size = new Size(258, 57);
+            BtnChooseFile.TabIndex = 1;
+            BtnChooseFile.Text = "Chọn file";
+            BtnChooseFile.Click += BtnChooseFile_Click;
             // 
-            // btnConvert
+            // BtnConvert
             // 
-            btnConvert.Location = new Point(40, 180);
-            btnConvert.Name = "btnConvert";
-            btnConvert.Size = new Size(200, 40);
-            btnConvert.TabIndex = 3;
-            btnConvert.Text = "Convert";
-            btnConvert.Click += btnConvert_Click;
+            BtnConvert.Location = new Point(317, 227);
+            BtnConvert.Name = "BtnConvert";
+            BtnConvert.Size = new Size(258, 57);
+            BtnConvert.TabIndex = 3;
+            BtnConvert.Text = "Convert";
+            BtnConvert.Click += BtnConvert_Click;
             // 
             // lblFile
             // 
-            lblFile.AutoSize = true;
-            lblFile.Location = new Point(98, 129);
+            lblFile.Font = new Font("Segoe UI", 12F);
+            lblFile.Location = new Point(373, 170);
             lblFile.Name = "lblFile";
-            lblFile.Size = new Size(84, 15);
+            lblFile.Size = new Size(142, 38);
             lblFile.TabIndex = 2;
             lblFile.Text = "Chưa chọn file";
             // 
+            // label1
+            // 
+            label1.Location = new Point(-1, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 33);
+            label1.TabIndex = 4;
+            label1.Text = "Chọn loại Convert:";
+            // 
             // Form1
             // 
-            ClientSize = new Size(300, 260);
+            ClientSize = new Size(894, 403);
+            Controls.Add(label1);
             Controls.Add(cbConvertType);
-            Controls.Add(btnChooseFile);
+            Controls.Add(BtnChooseFile);
             Controls.Add(lblFile);
-            Controls.Add(btnConvert);
+            Controls.Add(BtnConvert);
             Name = "Form1";
             Text = "File Converter";
             ResumeLayout(false);
-            PerformLayout();
         }
+        private Label label1;
     }
 }

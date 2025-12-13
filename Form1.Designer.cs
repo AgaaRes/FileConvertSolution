d3f509732fd5aa449d0dcb85a2827cd8425a951c
Nguyen Thien <phucthien8154@gmail.com>
2025-12-13 21:29:43 +0700

    ChangeF1Design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,43 +24,52 @@ namespace FileConverterGUI.Converters
             btnChooseFile = new Button();
             btnConvert = new Button();
             lblFile = new Label();
-
             SuspendLayout();
-
-            // ComboBox
+            // 
+            // cbConvertType
+            // 
             cbConvertType.DropDownStyle = ComboBoxStyle.DropDownList;
-            cbConvertType.Items.AddRange(new object[]
-            {
-                "DOCX → PDF",
-                "TXT → PDF"
-            });
-            cbConvertType.SelectedIndex = 0;
-            cbConvertType.Location = new Point(30, 30);
-            cbConvertType.Size = new Size(200, 30);
-
-            // Button Choose File
+            cbConvertType.Items.AddRange(new object[] { "DOCX → PDF", "TXT → PDF" });
+            cbConvertType.Location = new Point(40, 22);
+            cbConvertType.Name = "cbConvertType";
+            cbConvertType.Size = new Size(200, 23);
+            cbConvertType.TabIndex = 0;
+            // 
+            // btnChooseFile
+            // 
+            btnChooseFile.Location = new Point(12, 69);
+            btnChooseFile.Name = "btnChooseFile";
+            btnChooseFile.Size = new Size(258, 57);
+            btnChooseFile.TabIndex = 1;
             btnChooseFile.Text = "Chọn file";
-            btnChooseFile.Location = new Point(30, 80);
-            btnChooseFile.Size = new Size(200, 40);
             btnChooseFile.Click += btnChooseFile_Click;
-
-            // Label
-            lblFile.Text = "Chưa chọn file";
-            lblFile.AutoSize = true;
-            lblFile.Location = new Point(30, 140);
-
-            // Button Convert
-            btnConvert.Text = "Convert";
-            btnConvert.Location = new Point(30, 180);
+            // 
+            // btnConvert
+            // 
+            btnConvert.Location = new Point(40, 180);
+            btnConvert.Name = "btnConvert";
             btnConvert.Size = new Size(200, 40);
+            btnConvert.TabIndex = 3;
+            btnConvert.Text = "Convert";
             btnConvert.Click += btnConvert_Click;
-
-            // Form
+            // 
+            // lblFile
+            // 
+            lblFile.AutoSize = true;
+            lblFile.Location = new Point(98, 129);
+            lblFile.Name = "lblFile";
+            lblFile.Size = new Size(84, 15);
+            lblFile.TabIndex = 2;
+            lblFile.Text = "Chưa chọn file";
+            // 
+            // Form1
+            // 
             ClientSize = new Size(300, 260);
             Controls.Add(cbConvertType);
             Controls.Add(btnChooseFile);
             Controls.Add(lblFile);
             Controls.Add(btnConvert);
+            Name = "Form1";
             Text = "File Converter";
             ResumeLayout(false);
             PerformLayout();

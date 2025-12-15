98e5d0cb69bf942d5064310dfd5b1a2cfb0d5460
Thien Nguyen <phth0831@gmail.com>
2025-12-15 20:38:20 +0700

    Add files via upload

---
+++ Converters/PdfToJpgConverter.cs
@@ -0,0 +1,28 @@
+﻿using PdfiumViewer;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace FileConverterGUI.Converters
+{
+    public class PdfToJpgConverter
+    {
+        public void Convert(string pdfPath, string outputFolder)
+        {
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+
+            using (PdfDocument doc = PdfDocument.Load(pdfPath))
+            {
+                for (int i = 0; i < doc.PageCount; i++) 
+                {
+                    using Image img = doc.Render(i, 200, 200, true);
+                    img.Save(
+                        Path.Combine(outputFolder, $"page_{i + 1}.jpg"),
+                        ImageFormat.Jpeg
+                    );
+                }
+            }
+        }
+    }
+}

3994a6b1fd9df7ed241f8ba6f826bccade772d63
Thien Nguyen <phth0831@gmail.com>
2025-12-15 20:39:24 +0700

    Add files via upload

---
+++ Converters/PdfToDocxConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Spire.Pdf;
+
+namespace FileConverterGUI.Converters
+{
+    public class PdfToDocxConverter : IFileConverter
+    {
+        public string Convert(string inputPath, string outputDir)
+        {
+            Directory.CreateDirectory(outputDir);
+
+            string outputFile = Path.Combine(outputDir,
+                Path.GetFileNameWithoutExtension(inputPath) + ".docx");
+
+            PdfDocument pdf = new();
+            pdf.LoadFromFile(inputPath);
+            pdf.SaveToFile(outputFile, Spire.Pdf.FileFormat.DOCX);
+
+            return outputFile;
+        }
+    }
+}

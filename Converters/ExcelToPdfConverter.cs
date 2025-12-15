765468986bbe09b8bcc7b15008865773e206504f
Thien Nguyen <phth0831@gmail.com>
2025-12-15 20:37:54 +0700

    Add files via upload

---
+++ Converters/ExcelToPdfConverter.cs
@@ -0,0 +1,15 @@
+﻿using Spire.Xls;
+
+namespace FileConverterGUI.Converters
+{
+    public class ExcelToPdfConverter
+    {
+        public void Convert(string inputPath, string outputPath)
+        {
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(inputPath);
+
+            workbook.SaveToFile(outputPath, FileFormat.PDF);
+        }
+    }
+}
